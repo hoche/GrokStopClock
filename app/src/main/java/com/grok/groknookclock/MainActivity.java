9d2358c4010b5f4c4fa3f302a646ec5b90899ad7
Michel Hoche-Mong <hoche@grok.com>
2018-06-01 00:35:35 -0700

    Basic clock layout implemented.

--- app/src/main/java/com/grok/groknookclock/MainActivity.java
+++ app/src/main/java/com/grok/groknookclock/MainActivity.java
@@ -1,7 +1,12 @@
 package com.grok.groknookclock;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +14,30 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TableLayout tl = (TableLayout)findViewById(R.id.time_table);
+        LayoutInflater inflater = getLayoutInflater();
+
+        for(int i = 0; i < 9; i++) {
+            TableRow row = (TableRow)inflater.inflate(R.layout.time_table_row,
+                    tl, false);
+
+            TextView entry_num = (TextView)row.findViewById(R.id.entry_number);
+            TextView entry_time = (TextView)row.findViewById(R.id.entry_time);
+
+            if (i == 0) {
+                // title row
+                row.setBackgroundColor(Color.GRAY);
+                entry_num.setText("ID");
+                entry_time.setText("Time");
+            } else {
+                entry_num.setText("" + i);
+                entry_time.setText("00:00:0" + i); // set the text for the header
+            }
+            // other customizations to the row
+
+            tl.addView(row);
+        }
+
     }
 }
