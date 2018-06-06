41181beae0f891f7b8dbebdbb442e88b81f7b971
Michel Hoche-Mong <hoche@grok.com>
2018-06-06 12:13:48 -0700

    Rearranged the main display a bit. Added some icons.

--- app/src/main/java/com/grok/stopclock/SettingsActivity.java
+++ app/src/main/java/com/grok/stopclock/SettingsActivity.java
@@ -1,15 +1,19 @@
 package com.grok.stopclock;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 
 import com.grok.stopclock.R;
 
-public class SettingsActivity extends PreferenceActivity {
+public class SettingsActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.preferences);
+
+        setContentView(R.layout.activity_settings);
+
+        //addPreferencesFromResource(R.xml.preferences);
 
         /*
         // to handle a custom preference
